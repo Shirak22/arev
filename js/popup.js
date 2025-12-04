af99e0970b39ad47cbabe8dd9d6d8249024c79a2
Shirak Soghomonian <shirag_serop_6@hotmail.com>
2025-12-04 13:48:55 +0100

    add dynamic weather data to popup window

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,82 @@
-const renderPopup = (weather) => {
-    const popupContainer = document.querySelector('#popup-container');
-    const popupWeatherDescription = document.querySelector('#popup-weather-description');
-    const popupWeatherTemperature = document.querySelector('#popup-weather-temperature');
-    const popupWeatherTemperatureValue = document.querySelector('.weather-temperature-value');
-    const popupWeatherTemperatureFeelsLike = document.querySelector('.weather-temperature-feels-like');
-    const popupWeatherTemperatureWindSpeed = document.querySelector('.weather-temperature-wind-speed');
-    const popupWeatherTemperatureHumidity = document.querySelector('.weather-temperature-humidity');
+import { getFromCache, storeToCache } from "./cache.js";
+import render from "./render/render.js";
+import { loadWeatherIconsMapping } from "./weather/weather-icon.js";
+import renderForecast from "./weather/weather-forecast.js";
+import { renderPopup } from "./render/render-popup.js";
 
-    popupWeatherDescription.innerHTML = `
-    <img src="assets/icons/svg/star.svg" alt="Weather Icon">
-    <p>${weather.current.weathercode}</p>
-    `;
-    popupWeatherTemperature.innerHTML = `
-    <h2>${weather.current.temperature_2m}°</h2>
-    <p>Feels like ${weather.current.apparent_temperature}°C</p>
-    <p>WS ${weather.current.wind_speed_10m} m/s</p>
-    <p>H ${weather.current.relative_humidity_2m}%</p>
-    `;  
+
+
+
+async function getPositionByGeolocation() {
+   const position = new Promise((resolve, reject)=> {
+    try {   
+         navigator.geolocation.getCurrentPosition((position)=> {
+        resolve(position);
+        });
+    } catch (error) {
+        reject(error);
+    }
+   }) 
+   const getPosition = await position; 
+  const location = await chrome.runtime.sendMessage({type: 'getLocationByCoords', latitude:getPosition.coords.latitude, longitude: getPosition.coords.longitude})
+  return  location;
 }
 
-export default renderPopup;
+async function getLocationByIp() {
+    const response = await chrome.runtime.sendMessage({ type: 'getLocation' });
+    if(response.error) return null;
+    return response;
+}
+
+async function getWeatherByCoords(latitude, longitude) {
+    if(!latitude || !longitude){
+        console.error("coords are not correct... "); 
+        return; 
+    }
+    const response = await chrome.runtime.sendMessage({ type: 'getWeatherByCoords', latitude: latitude, longitude: longitude });
+    if(response) {
+        return response;
+    }else {
+        return null;
+    }
+}
+
+
+
+
+let position = null; 
+let weather = null;
+let cachedPosition = null ; 
+let cachedWeather = null; 
+
+const setup = async () => {
+  // Ensure weather icons mapping is loaded before rendering
+  await loadWeatherIconsMapping();
+  
+  cachedPosition  = await getFromCache('position');
+  cachedWeather = await getFromCache('weather'); 
+
+  if(!cachedPosition){
+    position = await getLocationByIp();
+    if(!position) position = await getPositionByGeolocation(); 
+    position && await storeToCache('position', position); 
+  }else {
+    position = cachedPosition.position; 
+  }
+
+    if (!cachedWeather) {
+        weather = await getWeatherByCoords(position.latitude, position.longitude);
+        weather && await storeToCache('weather', weather);
+    } else {
+        weather = cachedWeather.weather;
+    }
+
+    console.log(position);
+    console.log(weather || null);
+    
+    renderPopup( weather || null);
+
+    
+}   
+
+setup();
---
+++ js/render/render-popup.js
@@ -0,0 +1,21 @@
+import getWeatherIcon from "../weather/weather-icon.js";
+import getWeatherDescription from "../weather/weather-description.js";
+
+export const renderPopup = (weather) => {
+    const popupContainer = document.querySelector('#popup-container');
+    const popupWeatherDescription = document.querySelector('#popup-weather-description');
+    const popupWeatherTemperature = document.querySelector('#popup-weather-temperature');
+
+
+    console.log(weather);
+    popupWeatherDescription.innerHTML = `
+    <img src="assets/icons/svg/${getWeatherIcon(weather.current.weathercode)}" alt="Weather Icon">
+    <p>${getWeatherDescription(weather.current.weathercode)}</p>
+    `;
+    popupWeatherTemperature.innerHTML = `
+    <h2>${weather.current.temperature_2m}°C</h2>
+    <p>Feels like ${weather.current.apparent_temperature}°C</p>
+    <p>WS ${weather.current.wind_speed_10m} m/s</p>
+    <p>H ${weather.current.relative_humidity_2m}%</p>
+`;
+}
